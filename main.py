9c493b94faca577a069eb78d161b273fc7356156
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-11-24 20:48:19 +0100

    afegit main.py, cal provar-lo encara

---
+++ main.py
@@ -0,0 +1,31 @@
+#Importem els quatre arxius que hem creat per a fer el CRUD
+import create
+import delete
+import read
+import update
+
+valor = input("Quina funcio vols provar? \n1) Crear taula \n2) Llegir taula \n3)Modificar dades \n4)Inserir dades \n5)Borrar dades")
+
+if (valor == 1):
+    #Cridem a la funcio que crea la taula...
+    create.crea_taula()
+    read.read()
+
+elif (valor ==2):
+    #La funcio que llegeix els valors... (la repetim per a que surti la taula)
+    read.read()
+
+elif (valor==3):
+    #La que fa un update...
+    update.actualizar()
+
+elif (valor==4):
+    #Cridem a la funcio que inserta valors...
+    update.insert_values()
+
+elif (input==5):
+    #I la que elimina valors
+    delete.eliminar()
+
+else:
+    print("Siusplau introdueix un valor entre 1 i 5")
