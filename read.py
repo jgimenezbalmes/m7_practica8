ffef9ff4d529c24daeb68c34494e8e7c24df732e
Reymon <rvega@jaumebalmes.net>
2022-11-29 19:56:15 +0100

    Arreglos Read.py

--- read.py
+++ read.py
@@ -22,16 +22,16 @@ def read():
         print("Tabla Personas: ")
         for row in personas_existentes:
             print(row)
-        # Cerramos el Cursos en suo
+        # Cerramos el Curso en suo
         cursor.close()
-        # Hacemos un cambio de los cambios realizados (en este caso, la seleccion de la Tabala "personas")
+        # Hacemos un cambio de los cambios realizados (en este caso, la selección de la Tabala "personas")
         connexio.commit()
     # En cas que es doni error, guardem el text d'error que ens dona a la variable "error", i fem print
+    # Si se da el caso de saltarnos un error, guardaremos el mensaje de error
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    # Finalment, si la connexió no s'ha tancat, la tanquem
+        # Finalment, si la connexió no s'ha tancat, la tanquem
     finally:
         if connexio is not None:
             connexio.close()
-read()
 
